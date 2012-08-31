a2a45dc13df18c6aef683b4390c987f9319469ef
Marco Bonetti <mbonetti@gmail.com>
2012-08-31 21:34:44 +0200

    duplicity backup script

---
+++ duplicity.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# A duplicity script that backs up /etc and /home to a remote ftp server
+
+DATE_TODAY=`date +%d`
+PASSPHRASE='FIXME' # GPG
+FTP_PASSWORD='FIXME'
+FTP_USER='FIXME'
+FTP_HOST='FIXME'
+FTP_URL="$FTP_USER@$FTP_HOST"
+
+export FTP_URL
+export FTP_PASSWORD
+export PASSPHRASE
+
+###############################################
+
+mkdir -p /var/log/duplicity
+
+date >>/var/log/duplicity/etc.log
+date >>/var/log/duplicity/home.log
+
+duplicity remove-older-than 2M -v5 --allow-source-mismatch ftp://$FTP_URL/etc >>/var/log/duplicity/etc.log
+duplicity remove-older-than 2M -v5 --allow-source-mismatch ftp://$FTP_URL/home >>/var/log/duplicity/home.log
+
+if [ $DATE_TODAY = 01 ]
+then
+        duplicity full -v5 --allow-source-mismatch /etc  ftp://$FTP_URL/etc >>/var/log/duplicity/etc.log
+        duplicity full -v5 --allow-source-mismatch /home ftp://$FTP_URL/home >>/var/log/duplicity/home.log
+else
+        duplicity incremental -v5 --allow-source-mismatch /etc ftp://$FTP_URL/etc >>/var/log/duplicity/etc.log
+        duplicity incremental -v5 --allow-source-mismatch /home ftp://$FTP_URL/home >>/var/log/duplicity/home.log
+fi
+
+unset FTP_URL
+unset PASSPHRASE
+unset FTP_PASSWORD
+
+exit 0
+
