cf79517c5fa7fce06a69817155ee6f99df47d967
Marco Bonetti <mbonetti@gmail.com>
2012-10-20 16:03:51 +0300

    made the supervisord init.d script slightly more robust

--- provision.sh
+++ provision.sh
@@ -50,6 +50,7 @@ sudo mv supervisord.conf /etc
 wget https://raw.github.com/Supervisor/initscripts/master/debian-norrgard
 sed -i 's/DAEMON=\/usr\/bin/DAEMON=\/usr\/local\/bin/g' debian-norrgard
 sed -i 's/SUPERVISORCTL=\/usr\/bin/SUPERVISORCTL=\/usr\/local\/bin/g' debian-norrgard
+sed -i 's/DAEMON_ARGS="--pidfile \${PIDFILE}"/DAEMON_ARGS="--pidfile \${PIDFILE} -c \/etc\/supervisord.conf"/g' debian-norrgard
 
 sudo mv debian-norrgard /etc/init.d/supervisord
 sudo chmod +x /etc/init.d/supervisord
