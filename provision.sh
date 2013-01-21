3f576eaffd9a329f115f3fa363cc0342462b542e
Marco Bonetti <mbonetti@gmail.com>
2013-01-21 15:15:08 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -59,21 +59,21 @@ kernel.shmall=2097152
 EO_CONF
 
 cat > 99-network-tuning.conf <<EO_CONF
-net.ipv4.ip_local_port_range='1024 65000'
-net.ipv4.tcp_tw_reuse='1'
-net.ipv4.tcp_fin_timeout='15'
-net.core.netdev_max_backlog='4096'
-net.core.rmem_max='16777216'
-net.core.somaxconn='4096'
-net.core.wmem_max='16777216'
-net.ipv4.tcp_max_syn_backlog='20480'
-net.ipv4.tcp_max_tw_buckets='400000'
-net.ipv4.tcp_no_metrics_save='1'
-net.ipv4.tcp_rmem='4096 87380 16777216'
-net.ipv4.tcp_syn_retries='2'
-net.ipv4.tcp_synack_retries='2'
-net.ipv4.tcp_wmem='4096 65536 16777216'
-vm.min_free_kbytes='65536'
+net.ipv4.ip_local_port_range=1024 65000
+net.ipv4.tcp_tw_reuse=1
+net.ipv4.tcp_fin_timeout=15
+net.core.netdev_max_backlog=4096
+net.core.rmem_max=16777216
+net.core.somaxconn=4096
+net.core.wmem_max=16777216
+net.ipv4.tcp_max_syn_backlog=20480
+net.ipv4.tcp_max_tw_buckets=400000
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_rmem=4096 87380 16777216
+net.ipv4.tcp_syn_retries=2
+net.ipv4.tcp_synack_retries=2
+net.ipv4.tcp_wmem=4096 65536 16777216
+vm.min_free_kbytes=65536
 EO_CONF
 
 sudo mv 98-mem-tuning.conf 99-network-tuning.conf /etc/sysctl.d/
