f58f68e80ac2d6eea399639ff8fbd10e566f1477
Marco Bonetti <mbonetti@gmail.com>
2013-03-03 15:31:01 +0100

    dump the new user's pubkey

--- user_add.sh
+++ user_add.sh
@@ -4,6 +4,11 @@ mkdir -p /home/projects
 if [ ! -d "/home/projects/$username" ]; then
         useradd -b /home/projects -m -s /bin/zsh $username
         sudo -u $username -H ssh-keygen -q -N '' -t rsa -f /home/projects/$username/.ssh/id_rsa
+        clear
+        echo "Added $username"
+        echo ""
+        cat /home/projects/$username/.ssh/id_rsa.pub 
+        echo ""
 else
         echo "dir exists"
 fi
