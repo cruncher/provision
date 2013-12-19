97f08a97bd6b24309a6b92f1b3712db3dd7ac712
Marco Bonetti <mbonetti@gmail.com>
2013-12-19 10:46:37 +0100

    Update user_add.sh

--- user_add.sh
+++ user_add.sh
@@ -4,6 +4,8 @@ mkdir -p /home/projects
 if [ ! -d "/home/projects/$username" ]; then
         useradd -b /home/projects -m -s /bin/zsh $username
         sudo -u $username -H ssh-keygen -q -N '' -t rsa -f /home/projects/$username/.ssh/id_rsa
+        sudo -u $username git config --global user.name '$username deploy server'
+        sudo -u $username git config --global user.email 'info@cruncher.ch'
         clear
         echo "Added $username"
         echo ""
