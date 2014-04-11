37a0dd8a0b0e7746bc25c682dc4c4983797ee9c3
Marco Bonetti <mbonetti@gmail.com>
2014-04-11 15:02:01 +0200

    removed postgis stuff

--- provision.sh
+++ provision.sh
@@ -35,17 +35,6 @@ ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib
 ln -s /usr/lib/x86_64-linux-gnu/libfreetype.so /usr/lib
 ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib
 
-# postgis template
-sudo su postgres -c'createdb -E UTF8 -U postgres template_postgis'
-sudo su postgres -c'createlang -d template_postgis plpgsql;'
-sudo su postgres -c'psql -U postgres -d template_postgis -c"CREATE EXTENSION hstore;"'
-sudo su postgres -c'psql -U postgres -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql'
-sudo su postgres -c'psql -U postgres -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql'
-sudo su postgres -c'psql -U postgres -d template_postgis -c"select postgis_lib_version();"'
-sudo su postgres -c'psql -U postgres -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;"'
-sudo su postgres -c'psql -U postgres -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"'
-sudo su postgres -c'psql -U postgres -d template_postgis -c "GRANT ALL ON geography_columns TO PUBLIC;"'
-
 
 ## SUPERVISORD ##
 pip install supervisor
