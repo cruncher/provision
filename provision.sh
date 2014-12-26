4d89b8ab87b6ef913dae50bebb30925168a632a6
Marco Bonetti <mbonetti@gmail.com>
2014-12-26 14:39:17 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -78,6 +78,7 @@ sed -i 's/DAEMON=\/usr\/bin/DAEMON=\/usr\/local\/bin/g' debian-norrgard
 sed -i 's/SUPERVISORCTL=\/usr\/bin/SUPERVISORCTL=\/usr\/local\/bin/g' debian-norrgard
 sed -i 's/DAEMON_ARGS="--pidfile \${PIDFILE}"/DAEMON_ARGS="--pidfile \${PIDFILE} -c \/etc\/supervisord.conf"/g' debian-norrgard
 sed -i 's/# server_names_hash_bucket_size 64/server_names_hash_bucket_size 64/g' /etc/nginx/nginx.conf
+rm /etc/nginx/sites-enabled/default
 mv debian-norrgard /etc/init.d/supervisord
 chmod +x /etc/init.d/supervisord
 update-rc.d supervisord defaults
