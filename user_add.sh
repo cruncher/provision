a2e6fbb4e701bad5dbca21e29bdf1b1a9ce122b2
Marco Bonetti <mbonetti@gmail.com>
2015-04-05 17:51:25 +0200

    Update user_add.sh

--- user_add.sh
+++ user_add.sh
@@ -3,16 +3,15 @@ read -p "username? " username
 mkdir -p /home/projects
 if [ ! -d "/home/projects/$username" ]; then
         useradd -b /home/projects -m -s /bin/zsh $username
-        sudo -u $username -H ssh-keygen -q -N '' -t rsa -f /home/projects/$username/.ssh/id_rsa
-        sudo -u $username git config --global user.name '$username deploy server'
-        sudo -u $username git config --global user.email 'info@cruncher.ch'
-        su -c 'createuser -drS $username' - postgres
+        su -c "ssh-keygen -q -N '' -t rsa -f /home/projects/$username/.ssh/id_rsa" - $username
+        su -c "git config --global user.name '$username deploy server'" - $username
+        su -c "git config --global user.email 'info@cruncher.ch'" - $username
+        su -c "createuser -drS $username" - postgres
         clear
         echo "Added $username"
         echo ""
-        cat /home/projects/$username/.ssh/id_rsa.pub 
+        cat /home/projects/$username/.ssh/id_rsa.pub
         echo ""
 else
         echo "dir exists"
 fi
-
