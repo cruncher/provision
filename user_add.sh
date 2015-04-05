ba29784034365858d5a3b02973dcba6a16b26cfd
Marco Bonetti <mbonetti@gmail.com>
2015-04-05 17:46:49 +0200

    Update user_add.sh

--- user_add.sh
+++ user_add.sh
@@ -6,6 +6,7 @@ if [ ! -d "/home/projects/$username" ]; then
         sudo -u $username -H ssh-keygen -q -N '' -t rsa -f /home/projects/$username/.ssh/id_rsa
         sudo -u $username git config --global user.name '$username deploy server'
         sudo -u $username git config --global user.email 'info@cruncher.ch'
+        su -c 'createuser -drS $username' - postgres
         clear
         echo "Added $username"
         echo ""
