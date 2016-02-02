7b5f0c6b4fd9f45d1d1b3a95d18490f64e12fe66
Marco Bonetti <mbonetti@gmail.com>
2016-02-02 12:09:57 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -150,7 +150,7 @@ if [ "$yn_fw" = "y" ]; then
 fi
 
 
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 /etc/init.d/ssh restart
 
 cd
