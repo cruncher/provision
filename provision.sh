08045a8e2dc00bf2bcd37b229057f869c0faf9b5
Marco Bonetti <marco@cruncher.ch>
2018-11-13 16:38:42 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -41,7 +41,7 @@ apt-get -y dist-upgrade
 apt-get install -y sudo curl
 # curl -sL https://deb.nodesource.com/setup_0.12 | bash -
 # curl -sL https://deb.nodesource.com/setup_5.x | bash -
-curl -sL https://deb.nodesource.com/setup_7.x | bash -
+# curl -sL https://deb.nodesource.com/setup_7.x | bash -
 curl -sL https://deb.nodesource.com/setup_9.x | bash -
 
 ## SYSCTL ##
