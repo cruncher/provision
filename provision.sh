d1cb6602cc71cf01764df3d21e00ee91aba549e6
Marco Bonetti <marco@cruncher.ch>
2019-07-27 08:52:30 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -38,7 +38,7 @@ fi
 apt-get -y update
 apt-get -y upgrade
 apt-get -y dist-upgrade
-apt-get install -y sudo curl gnupg-agent
+apt-get install -y sudo curl 
 # curl -sL https://deb.nodesource.com/setup_0.12 | bash -
 # curl -sL https://deb.nodesource.com/setup_5.x | bash -
 # curl -sL https://deb.nodesource.com/setup_7.x | bash -
