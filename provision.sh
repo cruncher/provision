3588058a40753da0de163c8b7672b7e0d9437993
Marco Bonetti <marco@cruncher.ch>
2019-07-27 09:26:03 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -79,7 +79,7 @@ pip install --upgrade pip virtualenv
 pip3 install --upgrade pip virtualenv
 pip install lockfile fasteners
 
-sudo -y apt-get install apt-transport-https ca-certificates curl gnupg2  software-properties-common
+apt-get -y install apt-transport-https ca-certificates curl gnupg2  software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-get -y update
