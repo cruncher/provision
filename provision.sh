5fd52a652b3deb1ebd2c15d67757873d7c0a12a8
Marco Bonetti <marco@cruncher.ch>
2019-07-27 09:16:46 +0200

    docker

--- provision.sh
+++ provision.sh
@@ -79,6 +79,12 @@ pip install --upgrade pip virtualenv
 pip3 install --upgrade pip virtualenv
 pip install lockfile fasteners
 
+sudo -y apt-get install apt-transport-https ca-certificates curl gnupg2  software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
 
 ## SUPERVISORD ##
 pip install supervisor
