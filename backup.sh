35709feca209618690ddd324725453484720b1ea
Marco Bonetti <marco@cruncher.ch>
2021-08-19 11:08:48 +0200

    ignore basic stuff

--- backup.sh
+++ backup.sh
@@ -3,13 +3,13 @@
 REMOTE_USER="BACKUPUSER@BACKUPHOST"
 
 ssh $REMOTE_USER mkdir -p backups
+
 REPO_URL="sftp:$REMOTE_USER:backups/"
 LOGFILE="/var/log/restic/`date +"%Y%m%d-%H%M%S"`.log"
 
 mkdir -p /var/log/restic
 touch $LOGFILE
 
-# Make sure the repo exists, create it otherwise
 /usr/bin/restic \
         --quiet --repo $REPO_URL \
         --password-file=/root/.restic-password snapshots >> $LOGFILE 2>&1 \
@@ -19,18 +19,16 @@ touch $LOGFILE
         --password-file=/root/.restic-password init | tee -a $LOGFILE
 
 
-# Prune backups older than 45 days
 /usr/bin/restic \
         forget --keep-last 45  --prune \
         --password-file=/root/.restic-password \
         --repo $REPO_URL >> $LOGFILE 2>&1
 
-# Take a new snapshot
 /usr/bin/restic \
         --repo $REPO_URL \
         backup /home/projects \
         --password-file=/root/.restic-password \
         --exclude-file=/root/.restic-ignores \
+        --exclude=.venv --exclude=.pyenv --exclude="*.sock" \
         --exclude-caches \
         --verbose=0 >> $LOGFILE 2>&1
-
