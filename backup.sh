59feeb7a58387749dc7b5d7be8e4ba7071385ec2
Marco Bonetti <marco@cruncher.ch>
2023-02-03 11:37:55 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+set -e
+
+# SFTP:
 REMOTE_USER="BACKUPUSER@BACKUPHOST"
-
-ssh $REMOTE_USER mkdir -p backups
-
 REPO_URL="sftp:$REMOTE_USER:backups/"
+
+# S3:
+# export AWS_ACCESS_KEY_ID="bucket-access-key"
+# export AWS_SECRET_ACCESS_KEY="bucket-secret"
+# REPO_URL="s3:sos-ch-gva-2.exo.io/bucket-name"
+
+CANARY_HASH = 'abcdef-02134-abcdef-02134'
 LOGFILE="/var/log/restic/`date +"%Y%m%d-%H%M%S"`.log"
 
 mkdir -p /var/log/restic
 touch $LOGFILE
-find /var/log/restic/ -type f -mtime 50 -delete
+
+echo "Start: $(date)" >> $LOGFILE
+
 
 /usr/bin/restic \
         --quiet --repo $REPO_URL \
@@ -27,9 +36,15 @@ find /var/log/restic/ -type f -mtime 50 -delete
 
 /usr/bin/restic \
         --repo $REPO_URL \
-        backup /home/projects \
+        backup / \
         --password-file=/root/.restic-password \
         --exclude-file=/root/.restic-ignores \
         --exclude=.venv --exclude=.pyenv --exclude="*.sock" \
         --exclude-caches \
         --verbose=0 >> $LOGFILE 2>&1
+
+
+curl -s -o /dev/null  https://canary.cruncher.ch/report/$CANARY_HASH/?result=$LOGFILE   >> $LOGFILE 2>&1
+
+
+echo "Finished: $(date)" >> $LOGFILE
